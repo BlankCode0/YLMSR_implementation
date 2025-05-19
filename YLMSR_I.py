754f0585d394f93f2d271324925474bf416c4a82
Raj Christ Ronghang <128846365+BlankCode0@users.noreply.github.com>
2025-05-19 12:06:44 +0530

    Update and rename YLMSR_I.ipynb to YLMSR_I.py

---
+++ YLMSR_I.py
@@ -0,0 +1,16 @@
+!git clone https://github.com/BlankCode0/YLMSR_implementation.git
+
+%cd YLMSR_implementation
+
+# remove existing model if you want to retrain them from start
+!rm -rf models/policy_model
+!rm -rf models/ref_model
+
+# train policy model
+! python train_dpo.py
+
+# check if model is safe properly
+!ls models/policy_model/
+
+# run test to check the responce between ref model and policy model
+!python test_generate.py
